b564948ae4070ee1e478aa701d6286070ebe6503
Vinicius Espindola <vinicius.espindola@principia.net>
2024-11-11 17:02:32 -0400

    seila

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
       </Head>
 
       <main>
+        TESTEEEEEEEEEEEEEEEEEEEEEE
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
